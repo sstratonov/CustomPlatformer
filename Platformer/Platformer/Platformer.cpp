f8f4bf8189c7777e0db970592126107a48b07fd3
Alex <sstratonov.studrada@gmail.com>
2020-05-16 21:42:19 +0300

    added normal jump,fixed camera issues, fixed collision and removed amim.

--- Platformer/Platformer/Platformer.cpp
+++ Platformer/Platformer/Platformer.cpp
@@ -7,17 +7,48 @@
 
 using namespace sf;
 using namespace std;
+/*
+class Character {
+public: 
+	float dx, dy, w, h,x,y,speed,moveTimer;
+	int w, h, health;
+	bool life, playerOnground;
+	Texture texture;
+	Sprite sprite;
+	String name;
+	Character(Image &image,  float X, float Y, int W, int H, String Name) {
+		x = X;
+		y = y;
+		w = W;
+		h = H;
+		name = Name;
+		moveTimer = 0;
+		speed = 0;
+		health = 100;
+		dx = 0; dy = 0;
+		life = true;
+		playerOnground = false;
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);
+		sprite.setOrigin(w / 2, h / 2);
+	}
+};*/
 
 class Player { // класс игрока
 private:
 	float x, y; // координаты игрока,используются только через сеттеры
 public:
 	float dx, dy, w, h; // перемещение по осям , высота и ширина спрайта игрока
-	float speed = 0; // изначальная скорость игрока
-	int direction; // изначальное направление игрока, меняется через свитч и кейсы
+	float speed = 0 ; // изначальная скорость игрока
 	int score; // счет 
 	bool life;
+	bool playerOnGround;
 	int health;
+	enum  statePlayer
+	{
+		left,right,up,down,jump,stay
+	};
+	statePlayer state;
 	String File; // здесь хранится имя файла
 	Sprite sprite; // спрайт игрока
 	Image image; // картинка для спрайта
@@ -27,64 +58,127 @@ public:
 		File = F;
 		w = W;
 		h = H;
-		direction = 0;
 		score = 0;
 		health = 100;
+		dx = 0;
+		dy = 0;
 		life = true;
+		playerOnGround = false;
+		state = stay;
 		image.loadFromFile("../../Sprites/PNG/" + File);
 		texture.loadFromImage(image);
 		sprite.setTexture(texture);
 		x = X;
 		y = Y;
-		sprite.setTextureRect(IntRect(w,h, w, h)); // создаем персонажа 
+		sprite.setTextureRect(IntRect(0,8, w, h)); // создаем персонажа 
+		sprite.setOrigin(w / 2, h / 2);
 	}
 	void update(float time) { // функция обновления картинки игры, привязана к сфмл времени, не к процессору, что бы избежать разной скорости игры
-		switch (direction)
+		playercontrol();
+
+		switch (state)
 		{
-		case 0: dx = speed; dy = 0;
+		case right: dx = speed; 
 			break;
-		case 1: dx = -speed; dy = 0;
+		case left: dx = -speed;
 			break;
-		case 2: dx = 0, dy = speed;
+		case up: 
 			break;
-		case 3: dx = 0, dy = -speed;
+		case down:
 			break;
-		default:
+		case stay: 
 			break;
+		
 		}
 		x += dx * time; // перемещение координаты х за время,когда задаем расстояние перемещения за время, получаем новую координату
+		checkCollision(dx, 0);
 		y += dy * time; // то же, что и с иксом
+		checkCollision(0, dy);
+		sprite.setPosition(x + w / 2, y + h / 2); // запоминаем координаты
 		if (health <= 0)
 		{
 			life = false;
 		}
 		speed = 0; //после того как двигались - возвращаем скорость 0
-		sprite.setPosition(x, y); // запоминаем координаты
-		physicsmap(); // функция коллизии игрока и предметов
+	
+		dy = dy + 0.0015 * time;
+
+		
+		//physicsmap(); // функция коллизии игрока и предметов
 	}
-	void physicsmap() {
+	void playercontrol() {
+		if (Keyboard::isKeyPressed(Keyboard::A))
+		{
+			state = left;
+			speed = 0.1;
+			//currentframe += 0.007 * time;
+			//if (currentframe > 3) currentframe -= 3;
+			//Dino.sprite.setTextureRect(IntRect(66 * int(currentframe) + 66, 113, -66, 91));
+
+		}
+		if (Keyboard::isKeyPressed(Keyboard::D))
+		{
+			state = right;
+			speed = 0.1;
+			//currentframe += 0.005 * time;
+			//if (currentframe > 3) currentframe -= 3;
+			//Dino.sprite.setTextureRect(IntRect(66 * int(currentframe), 113, 66, 91));
+
+		}
+		if ((Keyboard::isKeyPressed(Keyboard::W)) && (playerOnGround))
+		{
+			
+			state = jump;
+			dy = -0.5;
+			playerOnGround = false;
+			//currentframe += 0.005 * time;
+			//if (currentframe > 5) currentframe -= 5;
+			//Dino.sprite.setTextureRect(IntRect(74 * int(currentframe), 222, 74, 96));
+
+		}
+		if (Keyboard::isKeyPressed(Keyboard::S))
+		{
+			state = down;
+			speed = 0.1;
+			//currentframe += 0.005 * time;
+			//if (currentframe > 3) currentframe -= 3;
+			//Dino.sprite.setTextureRect(IntRect(66 * int(currentframe), 113, 65, 91));
+
+		}
+	}
+	float getplayerx() { // сеттер на координату х для камеры и игрока
+		return x;
+	}
+	float getplayery() { //сеттер на координату у для камеры и игрока
+		return y;
+	}
+	void checkCollision(float Dx, float Dy) {
 		for (int i = y / 64; i < (y + h) / 64; i++) // проходимся по всем игрикам, /64 потому что берем самый левый квадратик, потом самый правый при условии меньше
 			for (int j = x / 64; j < (x + w) / 64; j++) // тоже самое,но для иксов
 			{
 				if (TileMap[i][j] == 'b' || TileMap [i][j] == '0')  // если элемент карты такой-то
 				{
-					if (dy > 0)
+					if (Dy > 0)
 					{
 						y = i * 64 - h; // не даем войти в текстуру,когда перемещаемся вниз, просто отбрасывая персонажа на его высоту
+						dy = 0;
+						playerOnGround = true;
 					}
 
-					if (dy < 0)
+					if (Dy < 0)
 					{
 						y = i * 64 + 64; //тоже самое дальше для всех направлений
+						dy = 0;
 					}
-					if (dx > 0)
+					if (Dx > 0)
 					{
 						x = j * 64 - w;
 					}
-					if (dx < 0)
+					if (Dx < 0)
 					{
 						x = j * 64 + 64;
 					}
+					
 				}
 				if (TileMap[i][j] == 'c')
 				{
@@ -105,12 +199,7 @@ public:
 				}
 			}
 	}
-	float getplayerx() { // сеттер на координату х для камеры и игрока
-		return x;
-	}
-	float getplayery() { //сеттер на координату у для камеры и игрока
-		return y;
-	}
+
 	
 	};
 
@@ -144,7 +233,7 @@ int main()
 	speechBubbleSprite.setTextureRect(IntRect(139,74,128,122));
 
 
-	Player Dino("DinoSpriteDoux.png", 65, 795, 65.0, 100.0);
+	Player Dino("DinoSpriteDoux.png",65, 295, 65.0, 90.0);
 
 	bool showLeveltext = true;
 	float currentframe = 0; // переменная,которая хранит кадр анимации спрайта
@@ -192,43 +281,6 @@ int main()
 		coordinatePlayerY = Dino.getplayery();
 		if (Dino.life == true)
 		{
-
-			if (Keyboard::isKeyPressed(Keyboard::A))
-			{
-				Dino.direction = 1;
-				Dino.speed = 0.1;
-				currentframe += 0.007 * time;
-				if (currentframe > 3) currentframe -= 3;
-				Dino.sprite.setTextureRect(IntRect(66 * int(currentframe) + 66, 113, -66, 91));
-
-			}
-			if (Keyboard::isKeyPressed(Keyboard::D))
-			{
-				Dino.direction = 0;
-				Dino.speed = 0.1;
-				currentframe += 0.005 * time;
-				if (currentframe > 3) currentframe -= 3;
-				Dino.sprite.setTextureRect(IntRect(66 * int(currentframe), 113, 66, 91));
-
-			}
-			if (Keyboard::isKeyPressed(Keyboard::W))
-			{
-				Dino.direction = 3;
-				Dino.speed = 0.1;
-				currentframe += 0.005 * time;
-				if (currentframe > 5) currentframe -= 5;
-				Dino.sprite.setTextureRect(IntRect(74 * int(currentframe), 222, 74, 96));
-
-			}
-			if (Keyboard::isKeyPressed(Keyboard::S))
-			{
-				Dino.direction = 2;
-				Dino.speed = 0.1;
-				currentframe += 0.005 * time;
-				if (currentframe > 3) currentframe -= 3;
-				Dino.sprite.setTextureRect(IntRect(66 * int(currentframe), 113, 65, 91));
-
-			}
 			getPlayerView(Dino.getplayerx(), Dino.getplayery());
 		}
 		
